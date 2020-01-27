a86adb477f0cd2a10f9441552d3eb5446cff0358
Tina Wu <tinawu0603@hotmail.com>
2020-01-27 01:23:22 -0500

    [user-admin] completed render, create, delete, edit functions

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,89 +1,103 @@
 (function () {
     var $usernameFld, $passwordFld;
-    var $removeBtn, $editBtn, $createBtn;
+    var $removeBtn, $editBtn, $updateBtn, $createBtn;
     var $firstNameFld, $lastNameFld, $roleFld;
     var $userRowTemplate, $tbody;
-    var $userList;
     var userService = new AdminUserServiceClient();
+    var curUserId;
 
     let users = []
-    let usernameFld = $("#usernameFld")
-    let $userTable = $("#user-table")
 
     // Retrieve DOM elements, bind action icons to event handlers
     function main() {
-        $userList = $("#userList")
         $usernameFld = $("#usernameFld")
+        $passwordFld = $("#passwordFld")
+        $firstNameFld = $("#firstNameFld")
+        $lastNameFld = $("#lastNameFld")
+        $roleFld = $("#roleFld")
         $createBtn = $("#createBtn")
         $createBtn.click(createUser)
+        $removeBtn = $("#removeBtn")
+        $removeBtn.click(deleteUser)
+        $updateBtn = $("#updateBtn")
+        $updateBtn.click(updateUser)
         $editBtn = $("#editBtn")
-        $editBtn.click(updateUser)
+        $userRowTemplate = $("#userRowTemplate")
+        $tbody = $("#tableBody")
 
         userService
             .findAllUsers()
             .then(theusers => {
                 users = theusers
-                renderUsers()
+                renderUsers(theusers)
             })
     }
 
     // Creates a user object and update list of users on server response
     function createUser() {
         const newUser = {
-            username: $usernameFld.val()
+            username: $usernameFld.val(),
+            password: $passwordFld.val(),
+            firstname: $firstNameFld.val(),
+            lastname: $lastNameFld.val(),
+            role: $roleFld.val(),
         }
-        $usernameFld.val("")
+        $usernameFld.val("");
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("Faculty");
 
         userService.createUser(newUser)
             .then((actualUser) => {
-                // users.push(actualUser)
-                // renderUsers()
+                users.push(actualUser)
                 findAllUsers()
             })
     }
 
+    // Retrieve all the users and passes response 
     function findAllUsers() {
         userService
             .findAllUsers()
             .then(theusers => {
                 users = theusers
-                renderUsers()
+                renderUsers(theusers)
             })
     }
 
-    function findUserById() { … }
+    // Retrieve a user by their ID
+    function findUserById() {
+        userService.findUserById(curUserId)
+            .then((actualuser) => {
+                renderUser(actualuser)
+            })
+    }
 
-    function deleteUser(index) {
-        let user = users[index]
-        let userId = user._id
-
-        userService.deleteUser(userId)
+    // Deletes a user
+    function deleteUser(user) {
+        const index = users.indexOf(user)
+        userService.deleteUser(user._id)
             .then(response => {
                 users.splice(index, 1)
-                renderUsers()
+                renderUsers(users)
             })
     }
 
-    function selectUser() { … }
-
-    let currentUserIndex = -1
-    function editUser(index) {
-        currentUserIndex = index
-        let user = users[index]
-        let userId = user._id
-
-        userService.findUserById(userId)
-            .then(actualUser => {
-                $usernameFld.val(actualUser.username)
-            })
-    }
-
+    // Reads new values and creates new user object
     function updateUser() {
         const updatedUser = {
-            username: $usernameFld.val()
+            username: $usernameFld.val(),
+            password: $passwordFld.val(),
+            firstname: $firstNameFld.val(),
+            lastname: $lastNameFld.val(),
+            role: $roleFld.val(),
         }
-        $usernameFld.val("")
-        updatedUser._id = users[currentUserIndex]._id
+        $usernameFld.val("");
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("Faculty");
+        updatedUser._id = curUserId
 
         userService.updateUser(updatedUser._id, updatedUser)
             .then((actualUser) => {
@@ -91,27 +105,64 @@
             })
     }
 
-    function renderUser(user) { … }
-
-    function renderUsers(users) {
-        $userList.empty()
-        for (let u in users) {
-            let user = users[u]
-
-            $deleteBtn = $("<button>Delete</button>")
-            $deleteBtn.click(() => deleteUser(u))
-
-            $editBtn = $("<button>Edit</button>")
-            $editBtn.click(() => editUser(u))
-
-            $li = $("<li>")
-            $li.append($deleteBtn)
-            $li.append($editBtn)
-            $li.append(user.username)
-            $userList.append($li)
-        }
+    // Updates the form with the given user's new properties
+    function renderUser(user) {
+        $usernameFld.val(user.username)
+        $firstNameFld.val(user.firstname)
+        $lastNameFld.val(user.lastname)
+        $roleFld.val(user.role)
     }
 
+    // Iterates over given users and populates the table
+    function renderUsers(users) {
+        console.log(users)
+        $tbody.empty();
+        $usernameFld.val("");
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("Faculty");
+
+        for (let u in users) {
+            user = users[u]
+            console.log("user ".concat(u, " ", user.username))
+
+            const userClone = $userRowTemplate.clone();
+            userClone.removeClass('wbdv-hidden');
+            userClone.find('.wbdv-username').html(user.username);
+            // userClone.find('.wbdv-password').html(user.password);
+            userClone.find('.wbdv-first-name').html(user.firstname);
+            userClone.find('.wbdv-last-name').html(user.lastname);
+            userClone.find('.wbdv-role').html(user.role);
+
+            let removeButtonId = "removeBtn-".concat(user._id);
+            let removeButton1 = "<button id=\"";
+            let removeButton2 = removeButton1.concat(removeButtonId);
+            let removeButton3 = removeButton2.concat("\" type=\"button\" class=\"btn wbdv-remove\"><img src=\"../css/x-black.svg\"></button>");
+
+            let editButtonId = "editBtn-".concat(user._id);
+            let editButton1 = "<button id=\"";
+            let editButton2 = editButton1.concat(editButtonId);
+            let editButton3 = editButton2.concat("\" type=\"button\" class=\"btn wbdv-edit\"><img src=\"../css/edit.svg\"></button>");
+
+            let userActions = "<span class=\"float-right\">".concat(removeButton3, editButton3, "</span>");
+            userClone.find('.wbdv-actions').html(userActions);
+
+            $tbody.append(userClone);
+            let newRemoveBtn = $("#removeBtn-".concat(user._id));
+            let newEditBtn = $("#editBtn-".concat(user._id));
+
+            newRemoveBtn.click(() => {
+                deleteUser(user.clone())
+            });
+
+            newEditBtn.click(function () {
+                curUserId = $(this).attr('id').slice(8)
+                findUserById();
+            });
+
+        }
+    }
     $(main)
 
 })()
